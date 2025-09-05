178c3c67167643f6626d880fff5b8f62dbd265b4
marouf ebrahimi <maroufibrahimi7264@gmail.com>
2025-09-05 20:15:00 +0430

    Tahere font added for Persian

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <>
+      <Navbar />
       <div className="input_container">
         <input
           autoFocus
